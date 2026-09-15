d702df375f7cd903ea7ea1d42ff74edeeab304b3
HappyDonuts101 <ethan.ping.song@gmail.com>
2026-09-14 23:29:00 -0700

    Add files via upload

---
+++ date.java
@@ -0,0 +1,19 @@
+public class date {
+
+public static void main(String[] args) {
+String day = "Monday"; 
+int date = 14;
+int year = 2026;
+String month = "September";
+System.out.println(day);
+System.out.println(date);
+System.out.println(year);
+System.out.println(month);
+System.out.println("American format: " + day + "," + " " + month + " " + date + "," + " " + year);
+System.out.println("European format: " + day + " " + date + " " + month + " " + year);
+
+}
+
+
+
+}
